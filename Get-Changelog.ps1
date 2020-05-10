6a27ccc9d3c3d2ed425c922bafebcf6f43ab8d87
pingzing <mcalistern@gmail.com>
2020-05-10 17:29:27 +0300

    Make changelog generator get tag before most recent as it's lower bound

--- Get-Changelog.ps1
+++ Get-Changelog.ps1
@@ -1,8 +1,9 @@
 # DevOps script to generate a changelog for use in AppCenter by looking at commits between the most recent two tags
-[string[]] $lastTag = git tag --sort=-version:refname | Select-Object -First 1;
-$changeLog = git log --oneline "HEAD...$($lastTag[0])";
+[string[]] $twoMostRecentTags = git tag --sort=-version:refname | Select-Object -First 2;
+$changeLog = git log --oneline "HEAD...$($twoMostRecentTags[1])";
 $changeLog = $changeLog -replace '`n', "$0D$0A"; # URL-encode newlines as CRLFs.
 if ([string]::IsNullOrWhiteSpace($changeLog)) {
     $changeLog = "No commits found.";
 }
+
 Write-Host "##vso[task.setvariable variable=changeLog]$($changeLog)";
