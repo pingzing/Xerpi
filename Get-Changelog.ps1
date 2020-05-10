6e593e1a71aa7aa088f099fb64a7344825be403a
pingzing <mcalistern@gmail.com>
2020-05-10 16:47:41 +0300

    Add debugging to changelog generator

--- Get-Changelog.ps1
+++ Get-Changelog.ps1
@@ -2,4 +2,5 @@
 [string[]] $lastTag = git tag --sort=-version:refname | Select-Object -First 1;
 $changeLog = git log --oneline "HEAD...$($lastTag[0])";
 $changeLog = $changeLog -replace '`n', "$0D$0A"; # URL-encode newlines as CRLFs.
+Write-Host "Saving the following changelog: $($changeLog)";
 Write-Host "##vso[task.setvariable variable=changeLog]$($changeLog)";
