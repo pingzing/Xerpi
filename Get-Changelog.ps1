8d0efd5e17a3c1948db95157f34d5f553aade5f4
pingzing <mcalistern@gmail.com>
2020-05-10 17:09:54 +0300

    Handle nulls in changelogs

--- Get-Changelog.ps1
+++ Get-Changelog.ps1
@@ -1,7 +1,8 @@
 # DevOps script to generate a changelog for use in AppCenter by looking at commits between the most recent two tags
 [string[]] $lastTag = git tag --sort=-version:refname | Select-Object -First 1;
-Write-Host "Found the following tags: $($lastTag)";
 $changeLog = git log --oneline "HEAD...$($lastTag[0])";
 $changeLog = $changeLog -replace '`n', "$0D$0A"; # URL-encode newlines as CRLFs.
-Write-Host "Saving the following changelog: $($changeLog)";
+if ([string]::IsNullOrWhiteSpace($changeLog)) {
+    $changeLog = "No commits found.";
+}
 Write-Host "##vso[task.setvariable variable=changeLog]$($changeLog)";
